a2889e146e8ab3bb5ce2281e891bb896da2b401f
Furqan Abid <thisismefurqan@gmail.com>
2023-07-18 23:23:52 +0500

    Formatted

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,33 @@
 export default function ListGroup() {
-    const items = ['New York', 'Rio De Janeiro', 'Tokyo',
-    'Berlin', 'Nairobi', 'Helsinki', 'Oslo', 'Moscow'];
+  let items = [
+    "New York",
+    "Rio De Janeiro",
+    "Tokyo",
+    "Berlin",
+    "Nairobi",
+    "Helsinki",
+    "Oslo",
+    "Moscow",
+  ];
 
   return (
     <>
       <h1>Cities</h1>
+      {items.length === 0 && (
+        <>
+          <p>No cities found</p>
+        </>
+      )}
       <ul className="list-group">
-        {items.map((item) => <li key={item} className="list-group-item">{item}</li>)}
+        {items.map((item) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => console.log("clicked!")}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
