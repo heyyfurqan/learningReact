67cfd1d583e1c22dbd1831a4beb45e6989ef0005
Furqan Abid <thisismefurqan@gmail.com>
2023-07-19 12:17:48 +0500

    Add alert visibility prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 export default function App() {
 
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Alert>
+      { alertVisible && <Alert onClose={() => setAlertVisible(false)}>
         Ayo Nigga <h3>What you doin' today?</h3>
-      </Alert>
-      <Button color="danger" onClick={() => console.log('Clicked')}>
+      </Alert>}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>
         Click Meee!
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,12 +2,16 @@ import { ReactNode } from "react";
 
 interface Props{
   children: ReactNode; // allows passing everything to props
+  onClose : () => void;
 }
 
-const Alert = ({children}: Props) => {
+const Alert = ({children, onClose}: Props) => {
   return (
-    <div className="alert alert-primary" role="alert">
+    <div className="alert alert-primary alert-dismissable fade show" role="alert">
     {children}
+    <button onClick={onClose} type="button" className="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
     </div>
   )
 }
