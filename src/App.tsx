92c26f3280593869b3a48745e4534fb0ef7d0e4f
Furqan Abid <thisismefurqan@gmail.com>
2023-07-19 12:03:36 +0500

    Add button with multiple colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
-
+import Button from "./components/Button";
 
 export default function App() {
 
@@ -9,6 +9,9 @@ export default function App() {
       <Alert>
         Ayo Nigga <h3>What you doin' today?</h3>
       </Alert>
+      <Button color="danger" onClick={() => console.log('Clicked')}>
+        Click Meee!
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface Props{
+    children: string;
+    onClick: () => void;
+    color?: 'primary' | 'secondary' |  'danger'; // optional prop
+}
+
+const Button = ({children, onClick, color = 'primary'}: Props) => {
+  return (
+    <button className={'btn btn-' + color} onClick={onClick}>{children}</button>
+  )
+}
+
+export default Button
