f97161d09bdfc88ab82d97844fad584ee2c34a9b
Furqan Abid <thisismefurqan@gmail.com>
2023-07-19 10:41:11 +0500

    Pass function via props on selected item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import ListGroup from "./components/ListGroup";
 
 export default function App() {
+
+  
   let items = [
     "New York",
     "Rio De Janeiro",
@@ -12,9 +14,14 @@ export default function App() {
     "Moscow",
   ];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
+  
   return (
     <div>
-      <ListGroup items={items} heading="Cities"/>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-interface Props{
+interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-export default function ListGroup({items, heading}: Props) {
-
+export default function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(2);
 
   return (
@@ -21,7 +21,10 @@ export default function ListGroup({items, heading}: Props) {
         {items.map((item, index) => (
           <li
             key={item}
-            onClick={() => {setSelectedIndex(index)}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
             className={
               selectedIndex === index
                 ? "list-group-item active"
