0af8af0d81890a5e41120f8a345277cf07291a38
Furqan Abid <thisismefurqan@gmail.com>
2023-07-20 11:50:04 +0500

    Add basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import ExpandableText from './components/ExpandableText';
-import './App.css'
-import Button from './components/Button/Button';
-import { useState } from 'react';
+import Form from "./components/Form";
 
 export default function App(){
   return(
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis in eum, incidunt magnam accusamus non enim sint, porro repudiandae fugiat delectus blanditiis nobis obcaecati minus praesentium et, quo similique necessitatibus illum. Aperiam molestiae recusandae nihil, delectus illum saepe. Corporis at facilis saepe est laborum similique eligendi, deleniti, iste dolor temporibus modi illum excepturi, ut consequatur ducimus cumque voluptates recusandae doloremque quas officiis voluptatem. Aspernatur dolor ipsum adipisci animi harum et eveniet accusamus esse eaque dolorem veniam atque ad accusantium quas laboriosam deserunt, mollitia, quae modi! Rerum laboriosam ipsam rem quam qui id, totam doloribus eius. Totam quasi eos quo neque.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
