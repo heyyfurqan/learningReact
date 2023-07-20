3fd37017adc3ff1409ef00dd135dce1ea9a8f354
Furqan Abid <thisismefurqan@gmail.com>
2023-07-20 12:33:38 +0500

    Add feature to log form input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Form = () => {
+
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const person = {name: '', age: 0}
+
   return (
-    <form>
+    <form onSubmit={(event) => 
+    {event.preventDefault();
+    if (nameRef.current !== null)
+        person.name = (nameRef.current.value)
+    if (ageRef.current !== null)
+        person.age = parseInt(ageRef.current.value)
+    console.log(person);
+    
+    }}>
+
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
             Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
     </form>
